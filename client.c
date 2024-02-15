6c7895d43832b91755d2c41a4e1f403b07ce879a
Cyanea <g3tsuzumi@gmail.com>
2024-02-15 18:42:47 +0900

    update

--- client.c
+++ client.c
@@ -6,13 +6,14 @@
 /*   By: shonakam <shonakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 17:01:58 by shonakam          #+#    #+#             */
-/*   Updated: 2024/02/14 12:18:39 by shonakam         ###   ########.fr       */
+/*   Updated: 2024/02/15 18:42:30 by shonakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "ft_printf/srcs/ft_printf.h"
 
 // avoid magic number
@@ -22,39 +23,40 @@ void	send_signals(unsigned char c, int pid)
 {
 	int	i;
 
-	i = 7;
+	i = (CHAR_BIT - 1);
 	while (i >= 0)
 	{
 		if (((c >> i) & 1) == 0)
-		{
 			kill(pid, SIGUSR1); 
-		}
 		else
-		{
 			kill(pid, SIGUSR2);
-		}
-		usleep(150);
+		usleep(250);
 		i--;
 	}
+}
+
+void	manage_str(pid_t pid, char *s)
+{
+	size_t	i;
+	
+	i = 0;
+	ft_printf("%s\n", s);
+	while (s[i] != '\0')
+		send_signals(s[i++], pid);
+	send_signals('\n', pid);
 }
 
 // topcmd >> Guard kill ppid 0,1,2
 int main(int ac, char **av)
 {
-	pid_t	pid;
-	size_t	i;
+	pid_t			pid;
 
 	if (ac == 3)
 	{
-		pid = ft_atoi(av[1]);
+		pid = (pid_t)ft_atoi(av[1]);
 		if (pid < 100)
 			exit(1);
-		i = 0;
-		while (av[2][i] != '\0')
-		{
-			send_signals(av[2][i++], pid);
-		}
-		send_signals('\n', pid);
+		manage_str(pid, av[2]);
 	}
 	else
 	{
--- server.c
+++ server.c
@@ -6,16 +6,15 @@
 /*   By: shonakam <shonakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 03:19:40 by shonakam          #+#    #+#             */
-/*   Updated: 2024/02/13 20:54:02 by shonakam         ###   ########.fr       */
+/*   Updated: 2024/02/15 18:38:48 by shonakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <limits.h>
 #include <signal.h>
 #include "ft_printf/srcs/ft_printf.h"
 
-// avoid magic number
-#define BIT_PATTERN_LENGTH 8
 
 typedef struct {
 	int		sig_c;
@@ -26,17 +25,24 @@ typedef struct {
 // share bit management using SIGNAL throughout the program.
 BitData g_bit_data;
 
-void	handle_signals(int sig)
+void	handle_signals(int sig, siginfo_t *info, void *context)
 {
+	(void)context;
+	(void)info;
+
 	if (sig == SIGUSR1)
 		g_bit_data.sig_c = (g_bit_data.sig_c << 1) | 0;
 	else if (sig == SIGUSR2)
 		g_bit_data.sig_c = (g_bit_data.sig_c << 1) | 1;
 
-	if (++g_bit_data.ascii == BIT_PATTERN_LENGTH) {
+	if (++g_bit_data.ascii == CHAR_BIT)
+	{
 		ft_printf("%c", (char)g_bit_data.sig_c);
 		g_bit_data.sig_c = 0;
 		g_bit_data.ascii = 0;
+		printf("xxxxxxxxxxxxxxxx\n");
+		// kill(info->si_pid, SIGUSR1);
+		usleep(100);
 	}
 }
 
@@ -44,20 +50,16 @@ int main(void)
 {
 	struct sigaction sa;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_handler = handle_signals;
-	sa.sa_flags = 0;
+	sa.sa_sigaction  = handle_signals;
+	sa.sa_flags = SA_SIGINFO;
 
 	ft_printf("The server process ID is:\033[32m %d\n", getpid());
 	ft_printf("\033[90mWaiting for a message...\033[0m\n");
 	
-	if (sigaction(SIGUSR1, &sa, NULL) == (-1))
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
 	{
-		ft_printf("SIGERR1\n");
-		exit(1);
-	}
-	if (sigaction(SIGUSR2, &sa, NULL) == (-1))
-	{
-		ft_printf("SIGERR2\n");
+		ft_printf("SIGERR\n");
 		exit(1);
 	}
 	while (1)
